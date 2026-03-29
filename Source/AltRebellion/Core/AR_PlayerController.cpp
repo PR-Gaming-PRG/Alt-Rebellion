e550d80bbf83badf1bc10440ae3f370571a83c29
mor80 <egmor80@gmail.com>
2026-03-29 19:32:06 +0300

    add enhanced input - imc, input actions, bp classes setup

--- Source/AltRebellion/Core/AR_PlayerController.cpp
+++ Source/AltRebellion/Core/AR_PlayerController.cpp
@@ -1,4 +1,6 @@
 #include "AR_PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 AAR_PlayerController::AAR_PlayerController()
 {
@@ -8,12 +10,20 @@ AAR_PlayerController::AAR_PlayerController()
 void AAR_PlayerController::BeginPlay()
 {
     Super::BeginPlay();
+
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+        ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+    {
+        if (DefaultMappingContext)
+        {
+            Subsystem->AddMappingContext(DefaultMappingContext, 0);
+        }
+    }
 }
 
 void AAR_PlayerController::TogglePause()
 {
     bIsPaused = !bIsPaused;
     SetPause(bIsPaused);
-    // Позже здесь: показать/скрыть меню паузы
     UE_LOG(LogTemp, Warning, TEXT("Pause: %s"), bIsPaused ? TEXT("ON") : TEXT("OFF"));
 }
--- Source/AltRebellion/Core/AR_PlayerController.h
+++ Source/AltRebellion/Core/AR_PlayerController.h
@@ -2,7 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputActionValue.h"
 #include "AR_PlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class ALTREBELLION_API AAR_PlayerController : public APlayerController
@@ -12,13 +16,41 @@ class ALTREBELLION_API AAR_PlayerController : public APlayerController
 public:
     AAR_PlayerController();
 
-    // Поставить/снять паузу
     UFUNCTION(BlueprintCallable, Category = "Input")
     void TogglePause();
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> MoveAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> LookAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> ShootAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> Ability1Action;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> Ability2Action;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> UltimateAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> InteractAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> PauseAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    TObjectPtr<UInputAction> DodgeAction;
 
 protected:
     virtual void BeginPlay() override;
 
-    // Флаг паузы
     bool bIsPaused = false;
 };
